5acd7d12f7d5904ccfc561531852c4844cb7e57f
Kalpana <gawadekalpana648@gmail.com>
2026-01-05 08:43:27 +0530

    Initial commit - LoginDemoCore project

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using LoginDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace LoginDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: Login Page
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Login Action
+        [HttpPost]
+        public IActionResult Login(UserModel model)
+        {
+            string connStr = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection con = new SqlConnection(connStr))
+            {
+                string query = "SELECT COUNT(*) FROM Users WHERE Username=@u AND Password=@p";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@u", model.Username);
+                cmd.Parameters.AddWithValue("@p", model.Password);
+
+                con.Open();
+                int count = (int)cmd.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Username or Password";
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
---
+++ SQLdata/DB & Users Table Create.sql
@@ -0,0 +1,13 @@
+--CREATE DATABASE LoginDB
+--GO
+--USE LoginDB
+
+--CREATE TABLE Users
+--(
+--    UserId INT IDENTITY(1,1) PRIMARY KEY,
+--    Username NVARCHAR(50) NOT NULL,
+--    Password NVARCHAR(50) NOT NULL
+--)
+
+--INSERT INTO Users (Username, Password)
+--VALUES ('admin', '12345')
